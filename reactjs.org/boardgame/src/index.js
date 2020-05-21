17fd070e3cadedf2ab6369a8d62d82a36ba0a0d7
root <romeubertho@gmail.com>
2020-05-21 13:48:37 -0300

    add reactjs tutorial

---
+++ reactjs.org/boardgame/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+// Rendering a react component
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
